951fea7b7d52d9a9a2286b6a785d0cf31461e243
YadiBelly <Yadisabell96@gmail.com>
2022-09-19 21:59:44 +0000

    updated

---
+++ src/front/js/pages/Signup.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Signup = () => {
+  return (
+    <>
+      <div class="mb-3 row">
+        <label for="staticEmail" class="col-sm-2 col-form-label">
+          Email
+        </label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="staticEmail" />
+        </div>
+      </div>
+      <div class="mb-3 row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">
+          Password
+        </label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" />
+        </div>
+      </div>
+    </>
+  );
+};
